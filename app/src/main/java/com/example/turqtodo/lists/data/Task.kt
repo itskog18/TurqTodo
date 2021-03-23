070c6ff6b36ba3c98b8f8f8bd2658fb3e236180b
Isak Tauder Thyholdt Skog <itskog18@uia.no>
2021-03-23 19:40:21 +0100

    Added more variables to both classes, and set up the LayoutBinding for the TodoLists.

---
+++ app/src/main/java/com/example/turqtodo/lists/data/Task.kt
@@ -0,0 +1,12 @@
+package com.example.turqtodo.lists.list
+
+class task {
+    private var placeholderTaskName = "placeholderTaskName"
+    val taskName:String = ""
+    val isCompleted: Int = 0
+    val taskId: Int = 0
+}
+
+/*
+data class Task(val taskName:String, val isCompleted:Int, val taskId:Int)
+*/
